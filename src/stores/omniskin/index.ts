cc4632d6c6b8cbc5258b2f6158ed0b4c5cf2c238
Bruno Andrade <bruno.faria.andrade@gmail.com>
2022-07-23 16:36:20 -0300

    Added initial store state

--- src/stores/omniskin/index.ts
+++ src/stores/omniskin/index.ts
@@ -5,7 +5,21 @@ import { api } from 'services/api'
 import { OminiskinState } from './types'
 
 export const useOmniskin = create<OminiskinState>((set) => ({
-   skins: [],
+   skins: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((fakeSkin) => ({
+      id: window.crypto.randomUUID(),
+      name: '',
+      float: 0,
+      image: '',
+      pattern: 0,
+      price: 0,
+      priceFormated: '',
+      quality: '',
+      store: {
+         icon: '',
+         name: '',
+         url: '',
+      },
+   })),
    isFetching: true,
    fetch: async (params) => {
       set(() => ({ isFetching: true }))
