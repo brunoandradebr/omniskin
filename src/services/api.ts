edbc56b4e1826f24ce833e05cea4464ea6633cb2
Bruno Andrade <bruno.faria.andrade@gmail.com>
2022-07-23 22:27:06 -0300

    Removed skinport integration

--- src/services/api.ts
+++ src/services/api.ts
@@ -122,15 +122,15 @@ export const api = {
       }))
 
       // neshastore skins
-      let sortOrder = 1
-      if (params.sort === 'price' && params.order === 'asc') sortOrder = 1
-      if (params.sort === 'price' && params.order === 'desc') sortOrder = 2
-      if (params.sort === 'float' && params.order === 'asc') sortOrder = 3
+      let neshaSortOrder = 1
+      if (params.sort === 'price' && params.order === 'asc') neshaSortOrder = 1
+      if (params.sort === 'price' && params.order === 'desc') neshaSortOrder = 2
+      if (params.sort === 'float' && params.order === 'asc') neshaSortOrder = 3
       // neshastore do not sort items by greater float, so, repeat order by lower price
-      if (params.sort === 'float' && params.order === 'desc') sortOrder = 1
+      if (params.sort === 'float' && params.order === 'desc') neshaSortOrder = 1
 
       const { data: neshastoreResponse } = await neshastore.get(
-         `?query=${params.name}&orderBy[]=${sortOrder}&limit=60`
+         `?query=${params.name}&orderBy[]=${neshaSortOrder}&limit=60`
       )
       const { items: neshastoreItems } = neshastoreResponse
 
