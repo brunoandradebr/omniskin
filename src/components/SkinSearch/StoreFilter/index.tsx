17f1b62d4ab49756f29ef3a479f8ef7975ad92f8
Bruno Andrade <bruno.faria.andrade@gmail.com>
2023-03-23 05:14:36 -0300

    updated store filter csmoney icon

--- src/components/SkinSearch/StoreFilter/index.tsx
+++ src/components/SkinSearch/StoreFilter/index.tsx
@@ -5,54 +5,53 @@ import { useOmniskin } from 'stores/omniskin'
 import { Container } from './style'
 
 const storeList = [
-   {
-      name: 'csmoney',
-      image: 'https://cs.money/img/favicon.png',
-   },
-   {
-      name: 'dmarket',
-      image: 'https://cdn-front-static.dmarket.com/prod/v1-209-2/assets/img/fav.ico',
-   },
-   {
-      name: 'neshastore',
-      image: 'https://i.ibb.co/MM51ZSZ/Logo-Nesha-sem-fundo.png',
-   },
-   {
-      name: 'dash',
-      image: 'https://raichu-uploads.s3.amazonaws.com/logo_dash-skins_mxE7PD.png',
-   },
+  {
+    name: 'csmoney',
+    image: 'https://cs.money/img/favicon.ico',
+  },
+  {
+    name: 'dmarket',
+    image: 'https://cdn-front-static.dmarket.com/prod/v1-209-2/assets/img/fav.ico',
+  },
+  {
+    name: 'neshastore',
+    image: 'https://i.ibb.co/MM51ZSZ/Logo-Nesha-sem-fundo.png',
+  },
+  {
+    name: 'dash',
+    image: 'https://raichu-uploads.s3.amazonaws.com/logo_dash-skins_mxE7PD.png',
+  },
 ]
 
 export const StoreFilter = () => {
-   const params = useOmniskin((state) => state.params)
-   const setParams = useOmniskin((state) => state.setParams)
+  const params = useOmniskin(state => state.params)
+  const setParams = useOmniskin(state => state.setParams)
 
-   const handleOnChange = (store: TStoreParam) => {
-      setParams({
-         page: 0,
-         stores: params?.stores?.includes(store)
-            ? params.stores.filter((oldStore) => oldStore !== store)
-            : [...(params.stores as []), store],
-      })
-   }
+  const handleOnChange = (store: TStoreParam) => {
+    setParams({
+      page: 0,
+      stores: params?.stores?.includes(store)
+        ? params.stores.filter(oldStore => oldStore !== store)
+        : [...(params.stores as []), store],
+    })
+  }
 
-   return (
-      <Container>
-         <div className='store-list'>
-            {storeList.map((store) => (
-               <div
-                  key={store.name}
-                  title={store.name}
-                  className={`store ${store.name} ${
-                     params.stores?.includes(store.name as TStoreParam) &&
-                     '--is-active'
-                  }`}
-                  onClick={handleOnChange.bind(this, store.name as TStoreParam)}
-               >
-                  <img src={store.image} />
-               </div>
-            ))}
-         </div>
-      </Container>
-   )
+  return (
+    <Container>
+      <div className="store-list">
+        {storeList.map(store => (
+          <div
+            key={store.name}
+            title={store.name}
+            className={`store ${store.name} ${
+              params.stores?.includes(store.name as TStoreParam) && '--is-active'
+            }`}
+            onClick={handleOnChange.bind(this, store.name as TStoreParam)}
+          >
+            <img src={store.image} />
+          </div>
+        ))}
+      </div>
+    </Container>
+  )
 }
