92c612057d00ba9a8f0c9c6cddee0a271ee4138e
Bruno Andrade <bruno.faria.andrade@gmail.com>
2023-11-06 23:55:52 -0300

    chore: typo proxy url protocol

--- src/services/dmarket.ts
+++ src/services/dmarket.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const dmarket = axios.create({
   baseURL:
-    'http://api.allorigins.win/raw?url=https://api.dmarket.com/exchange/v1/market/items',
+    'https://api.allorigins.win/raw?url=https://api.dmarket.com/exchange/v1/market/items',
 })
