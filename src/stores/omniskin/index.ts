c6b0f653df44c1d0023af5ee52329827c7188b48
Bruno Andrade <bruno.faria.andrade@gmail.com>
2024-05-12 22:16:16 -0300

    fix: default bayonet search

--- src/stores/omniskin/index.ts
+++ src/stores/omniskin/index.ts
@@ -6,76 +6,71 @@ import { api } from 'services/api'
 import { OminiskinState } from './types'
 
 const randomSkinName = [
-   'eagle',
-   'usp',
-   'm4a1',
-   'ak-47',
-   'butterfly',
-   'bayoneta',
-   'flip knife',
-   'cobalt skulls',
-   'glove',
+  'eagle',
+  'usp',
+  'ak-47',
+  'butterfly',
+  'bayonet',
+  'flip knife',
+  'cobalt skulls',
+  'glove',
 ]
 
 const randomIndex = Math.round(Math.random() * (randomSkinName.length - 1))
 
-export const useOmniskin = create<OminiskinState>((set) => ({
-   skins: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((fakeSkin) => ({
-      id: uuid(),
+export const useOmniskin = create<OminiskinState>(set => ({
+  skins: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(fakeSkin => ({
+    id: uuid(),
+    name: '',
+    float: 0,
+    image: '',
+    pattern: 0,
+    price: 0,
+    priceFormated: '',
+    quality: '',
+    store: {
+      icon: '',
       name: '',
-      float: 0,
-      image: '',
-      pattern: 0,
-      price: 0,
-      priceFormated: '',
-      quality: '',
-      store: {
-         icon: '',
-         name: '',
-         url: '',
-      },
-      inspect: '',
-      availableAt: 0,
-   })),
-   isFetching: true,
-   resultTotal: 0,
-   params: {
-      name: randomSkinName[randomIndex],
-      limit: 10,
-      page: 0,
-      order: 'desc',
-      sort: 'price',
-      stores: ['csmoney', 'dmarket', 'neshastore', 'dash'],
-   },
-   fetch: async (params, keepPrevious = false) => {
-      set(() => ({ isFetching: true }))
+      url: '',
+    },
+    inspect: '',
+    availableAt: 0,
+  })),
+  isFetching: true,
+  resultTotal: 0,
+  params: {
+    name: randomSkinName[randomIndex],
+    limit: 10,
+    page: 0,
+    order: 'desc',
+    sort: 'price',
+    stores: ['csmoney', 'dmarket', 'neshastore', 'dash'],
+  },
+  fetch: async (params, keepPrevious = false) => {
+    set(() => ({ isFetching: true }))
 
-      if (keepPrevious === false) localStorage.setItem('dmarketPage', '')
+    if (keepPrevious === false) localStorage.setItem('dmarketPage', '')
 
-      const skins = await api.get(params)
+    const skins = await api.get(params)
 
-      const resultTotal = skins.length
+    const resultTotal = skins.length
 
-      set((state) => ({
-         skins: keepPrevious
-            ? api.sortSkins(
-                 [...state.skins, ...skins],
-                 params?.sort ?? 'price',
-                 params?.order ?? 'desc'
-              )
-            : api.sortSkins(
-                 [...skins],
-                 params?.sort ?? 'price',
-                 params?.order ?? 'desc'
-              ),
-         isFetching: false,
-         resultTotal,
-      }))
-   },
-   setParams: (params) => {
-      set((state) => ({
-         ...state,
-         params: { ...state.params, ...params },
-      }))
-   },
+    set(state => ({
+      skins: keepPrevious
+        ? api.sortSkins(
+            [...state.skins, ...skins],
+            params?.sort ?? 'price',
+            params?.order ?? 'desc',
+          )
+        : api.sortSkins([...skins], params?.sort ?? 'price', params?.order ?? 'desc'),
+      isFetching: false,
+      resultTotal,
+    }))
+  },
+  setParams: params => {
+    set(state => ({
+      ...state,
+      params: { ...state.params, ...params },
+    }))
+  },
 }))
